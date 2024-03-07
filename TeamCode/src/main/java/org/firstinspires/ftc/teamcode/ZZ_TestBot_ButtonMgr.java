a77589c7733d2d6d0398d567991383038cd2c7f5
rhindle <knowlton@gmail.com>
2024-03-07 10:19:48 -0500

    Improvements to ButtonMgr (doubletap, etc.) and an opMode to test.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZZ_TestBot_ButtonMgr.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.Common.ButtonMgr;
+
+@TeleOp (name="ZZ_TestBot_ButtonMgr", group="Test")
+//@Disabled
+public class ZZ_TestBot_ButtonMgr extends LinearOpMode {
+
+   ButtonMgr buttonMgr;
+   int wasTappedCount;
+   int wasHeldCount;
+   int wasPressedCount;
+   int wasReleasedCount;
+   int wasSingleTappedCount;
+   int wasDoubleTappedCount;
+
+   @Override
+   public void runOpMode() {
+      buttonMgr = new ButtonMgr(this);
+
+      resetCounters();
+
+      while (!isStarted()) {
+         telemetry.addData(">", "Opmode Ready.");
+         telemetry.update();
+         sleep(100);
+      }
+
+      while (opModeIsActive()) {
+
+         buttonMgr.updateAll();
+
+         ButtonMgr.cButton button1 = new ButtonMgr.cButton(1, ButtonMgr.Buttons.x);
+         ButtonMgr.cButton button2 = new ButtonMgr.cButton(1, ButtonMgr.Buttons.y);
+         ButtonMgr.cButton buttonReset = new ButtonMgr.cButton(1, ButtonMgr.Buttons.b);
+
+         if(buttonMgr.wasPressed(buttonReset)) resetCounters();
+
+         if(buttonMgr.wasTapped(button1)) {
+            wasTappedCount++;
+         }
+         if(buttonMgr.wasSingleTapped(button1)) {
+            wasSingleTappedCount++;
+         }
+         if(buttonMgr.wasDoubleTapped(button1)) {
+            wasDoubleTappedCount++;
+         }
+         if(buttonMgr.wasPressed(button1)) {
+            wasPressedCount++;
+         }
+         if(buttonMgr.wasReleased(button1)) {
+            wasReleasedCount++;
+         }
+         if(buttonMgr.wasHeld(button1)) {
+            wasHeldCount++;
+         }
+
+         telemetry.addLine("X is primary button for events below");
+         telemetry.addLine("Y is secondary button (Pressed2)");
+         telemetry.addLine("B resets the counts");
+         telemetry.addLine();
+         telemetry.addData("Pressed Count_",wasPressedCount);
+         telemetry.addData("Released Count",wasReleasedCount);
+         telemetry.addData("Tapped Count_",wasTappedCount);
+         telemetry.addData("1x Tap Count_",wasSingleTappedCount);
+         telemetry.addData("2x Tap Count_",wasDoubleTappedCount);
+
+         telemetry.addData("Held Count___",wasHeldCount);
+
+         telemetry.addData("Pressed______", buttonMgr.isPressed(button1));
+         telemetry.addData("Held_________", buttonMgr.isHeld(button1));
+         telemetry.addData("Tap-Held_____", buttonMgr.isSingleTapHeld(button1));
+
+         telemetry.addData("Pressed2_____", buttonMgr.isPressed(button2));
+
+         telemetry.update();
+      }
+   }
+
+   void resetCounters() {
+      wasTappedCount = 0;
+      wasHeldCount = 0;
+      wasPressedCount = 0;
+      wasReleasedCount = 0;
+      wasSingleTappedCount = 0;
+      wasDoubleTappedCount = 0;
+   }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Common/ButtonMgr.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Common/ButtonMgr.java
@@ -11,6 +11,8 @@ public class ButtonMgr {
 
     public ControlData[] controlData;
 
+    int tapTime = 500;
+
     ////////////////
     //constructors//
     ////////////////
@@ -43,6 +45,69 @@ public class ButtonMgr {
         }
     }
 
+    public boolean wasPressed(cButton ctrlButton) {
+        return wasPressed(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean wasPressed(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].wasPressed;
+    }
+
+    public boolean wasReleased(cButton ctrlButton) {
+        return wasReleased(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean wasReleased(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].wasReleased;
+    }
+
+    public boolean wasTapped(cButton ctrlButton) {
+        return wasTapped(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean wasTapped(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].wasTapped;
+    }
+
+    public boolean isHeld(cButton ctrlButton) {
+        return isHeld(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean isHeld(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].isHeld;
+    }
+
+    public boolean isPressed(cButton ctrlButton) {
+        return isPressed(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean isPressed(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].lastStatus;
+    }
+
+    public boolean wasSingleTapped(cButton ctrlButton) {
+        return wasSingleTapped(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean wasSingleTapped(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].wasSingleTapped;
+    }
+
+    public boolean wasDoubleTapped(cButton ctrlButton) {
+        return wasDoubleTapped(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean wasDoubleTapped(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].wasDoubleTapped;
+    }
+
+    public boolean wasHeld(cButton ctrlButton) {
+        return wasHeld(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean wasHeld(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].wasHeld;
+    }
+
+    public boolean isSingleTapHeld(cButton ctrlButton) {
+        return isSingleTapHeld(ctrlButton.controller, ctrlButton.button);
+    }
+    public boolean isSingleTapHeld(int controller, Buttons button) {
+        return controlData[getIndex(controller, button)].isSingleTapHeld;
+    }
+
     int getIndex(int controller, Buttons button){
         //This converts an index of 0-27 based on the controller 1-2 and button 0-13
         if (controller < 1 || controller > 2) controller = 0; else controller--;
@@ -51,22 +116,6 @@ public class ButtonMgr {
 
     ControlData getAllData(int controller, Buttons button) {
         return controlData[getIndex(controller, button)];
-    }
-
-    public boolean wasPressed(int controller, Buttons button) {
-        return controlData[getIndex(controller, button)].pressed;
-    }
-    public boolean wasReleased(int controller, Buttons button) {
-        return controlData[getIndex(controller, button)].released;
-    }
-    public boolean wasTapped(int controller, Buttons button) {
-        return controlData[getIndex(controller, button)].tapped;
-    }
-    public boolean isHeld(int controller, Buttons button) {
-        return controlData[getIndex(controller, button)].held;
-    }
-    public boolean isPressed(int controller, Buttons button) {
-        return controlData[getIndex(controller, button)].lastStatus;
     }
 
     public enum Buttons {  //must match what is in getReading's switch block
@@ -86,16 +135,29 @@ public class ButtonMgr {
         right_stick_button;
     }
 
+    public static class cButton {
+        int controller;
+        Buttons button;
+        public cButton(int controller, Buttons button){
+            this.controller = controller;
+            this.button = button;
+        }
+    }
+
     class ControlData {
         int index;
         Buttons name;
         boolean lastStatus;
         long lastTime;
-        boolean tapped;
-        boolean doubleTapped;
-        boolean held;
-        boolean pressed;
-        boolean released;
+        boolean wasTapped;
+        boolean isHeld;
+        boolean wasHeld;
+        boolean wasPressed;
+        boolean wasReleased;
+        boolean wasSingleTapped;
+        boolean wasDoubleTapped;
+        boolean isSingleTapHeld;
+        char tapEventCounter;
 
         public void initData(int index)  //object
         {
@@ -103,11 +165,15 @@ public class ButtonMgr {
             name = Buttons.values()[index % Buttons.values().length];
             lastStatus = false;
             lastTime = System.currentTimeMillis();
-            tapped = false;
-            doubleTapped = false;
-            held = false;
-            pressed = false;
-            released = false;
+            wasTapped = false;
+            isHeld = false;
+            wasHeld = false;
+            wasPressed = false;
+            wasReleased = false;
+            wasSingleTapped = false;
+            wasDoubleTapped = false;
+            isSingleTapHeld = false;
+            tapEventCounter = 0;
         }
 
         boolean getReading(int index)
@@ -135,7 +201,7 @@ public class ButtonMgr {
                 case right_bumper:        return gpad.right_bumper;
                 case left_stick_button:   return gpad.left_stick_button;
                 case right_stick_button:  return gpad.right_stick_button;
-                default:                  return false;//something bad happened
+                default:                  return false;  //something bad happened
             }
         }
 
@@ -144,36 +210,62 @@ public class ButtonMgr {
             long currentTime = System.currentTimeMillis();
             long deltaTime = currentTime - lastTime;
             boolean currentState = getReading(index);
-            if (!lastStatus && currentState) {  // change from not pressed to pressed
-                pressed = true;          // this will last for one loop!
-                held = false;
-                tapped = false;
-                lastTime = currentTime;  // reset the time
-            } else {
-                pressed = false;
-            }
-            if (lastStatus && !currentState) {  // change from pressed to not pressed
-                released = true;         // this will last for one loop!
-                held = false;
-                if (deltaTime < 500) {
-                    tapped = true;
+
+            // clear all transient events
+            wasPressed = false;
+            wasReleased = false;
+            wasHeld = false;
+            wasTapped = false;
+            wasSingleTapped = false;
+            wasDoubleTapped = false;
+            isHeld = false;
+
+            if (!lastStatus && currentState) {   // change from not pressed to pressed
+                wasPressed = true;               // this will last for one loop!
+                if (deltaTime < tapTime) {       // indicates released for < tapTime
+                    if (tapEventCounter != 0) tapEventCounter++;
+                } else {
+                    tapEventCounter = 0;
                 }
-                lastTime = currentTime;  // reset the time
+                lastTime = currentTime;          // reset the time
             } else {
-                released = false;
+                //nothing
             }
-            if (lastStatus && currentState) {   // still held
-                tapped = false;
-                if (deltaTime >= 500) {
-                    held = true;
+            if (lastStatus && !currentState) {   // change from pressed to not pressed
+                wasReleased = true;              // this will last for one loop!
+                isSingleTapHeld = false;
+                if (deltaTime < tapTime) {
+                    wasTapped = true;
+                    tapEventCounter++;
+                    if (tapEventCounter == 3) {
+                        wasDoubleTapped = true;
+                        tapEventCounter = 0;
+                    }
+                } else {
+                    wasHeld = true;
+                    tapEventCounter = 0;
+                }
+                lastTime = currentTime;          // reset the time
+            } else {
+                //nothing
+            }
+            if (lastStatus && currentState) {    // still held
+                if (deltaTime >= tapTime) {
+                    isHeld = true;
+                    if (tapEventCounter == 2) {
+                        isSingleTapHeld = true;  // will reset when released
+                    }
+                    tapEventCounter = 0;
                 }
             }
             if (!lastStatus && !currentState) {  // still not held
-                tapped = false;
-                held = false;
+                if (deltaTime >= tapTime) {
+                    if (tapEventCounter == 1) {
+                        wasSingleTapped = true;
+                    }
+                    tapEventCounter = 0;
+                }
             }
-            //need something for doubleTap (put this off until proof-of-concept works)
-            //after all the checks are done...
             lastStatus = currentState;
         }
     }
